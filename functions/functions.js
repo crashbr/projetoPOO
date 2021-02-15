d068055b27d364c5e914161c8a1be756ded06efc
Erik Sobral <eriksobral@gmail.com>
2021-02-14 23:59:24 -0300

    Criadas as funções para validação de usuario

--- functions/functions.js
+++ functions/functions.js
@@ -1,30 +1,66 @@
 const fs = require('fs')
 
 
-    function validateString(value) {
-        if (typeof(value) !== 'string' && typeof(value) !== 'undefined') throw `O ${value} precisa ser uma string.`;
-        return;
-    }
+function validateString(value) {
+    if (typeof(value) !== 'string' && typeof(value) !== 'undefined') throw `O ${value} precisa ser uma string.`;
+    return;
+}
 
-    function insertDb(data){
-        fs.writeFileSync('./db/db.json', JSON.stringify(data, null, 4)),(err) => {
-            if(err) console.log(err)
+function insertDb(data){
+    fs.writeFileSync('./db/db.json', JSON.stringify(data, null, 4)),(err) => {
+        if(err) console.log(err)
+    }
+}
+
+function readDb(){
+    let dataDb = JSON.parse(fs.readFileSync('./db/db.json'))
+    return dataDb
+}
+
+function verifyIfCompanyExists(companiesList, company) {
+    if (companiesList.findIndex(companyName => companyName.name === company) >= 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+function verifyAdmin(companiesList, email, password) {
+    let checkAdmin = companiesList.filter((obj) => {
+        return obj.adminUser.email === email
+    })
+    if (checkAdmin.length > 0 ){
+        if(checkAdmin[0].adminUser.email === email && checkAdmin[0].adminUser.password === password){
+        return ('userAdmin')
         }
     }
-
-    function readDb(){
-        let dataDb = JSON.parse(fs.readFileSync('./db/db.json'))
-        return dataDb
     }
-
-    function verifyIfCompanyExists(companiesList, company) {
-        if (companiesList.findIndex(companyName => companyName.name === company) >= 0){
-            return true
-        } else {
-            return false
+    
+function verifyEmployee(companiesList, email, password) {
+    let checkEmployee = companiesList.filter((emp) => {
+        return emp.employees.find((empFinal) => {
+        return empFinal.email.includes(email)
+        })
+    })
+    if(checkEmployee.length > 0 ){
+        if(checkEmployee[0].employees[0].email === email && checkEmployee[0].employees[0].password === password){
+        return ('userEmployee')
         }
-        
     }
+}
+    
+function verifyUser(companiesList, email, password) {
+    if(verifyAdmin(companiesList, email,password) === 'userAdmin'){
+    return ('userAdmin')
+    } 
+    else if (verifyEmployee(companiesList, email, password) === 'userEmployee'){
+        return('userEmployee')
+    }
+    else if(companiesList, email == 'eriksobral@gmail.com', password == '123'){
+        return('userDev')
+    } else {
+        return ('User not found!')
+    }
+}
 
-
-module.exports = {validateString, insertDb, readDb, verifyIfCompanyExists}
+module.exports = {validateString, insertDb, readDb, verifyIfCompanyExists, verifyUser}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Company = require('./class/Company')
 const Employee = require('./class/Employee')
 const input = require('readline-sync')
-const {readDb, verifyIfCompanyExists} = require('./functions/Functions')
+const {readDb, verifyIfCompanyExists, verifyUser} = require('./functions/Functions')
 
 while(true){
     const getDb = readDb()
@@ -18,142 +18,149 @@ while(true){
     const emailAuth = input.question('E-mail: ')
     const passAuth = input.question('Senha: ')
 
-    
+    const checkUser = verifyUser(companiesList, emailAuth, passAuth)
 
-    if(emailAuth == 'eriksobral@gmail.com' && passAuth === '123'){
-        let optDev = 1
+    switch(checkUser){
 
-        while(optDev !== 0){
-            console.log(`
-            Bem vindo desenvolvedor
-        
-            Escolha uma das opções abaixo, caso não queira alterar direto no banco.
-        
-            1 - Criar nova empresa
-            2 - Alterar senha de admin
-            0 - Sair
-            `)
+        case 'userDev':
+            let optDev = 1
 
-            optDev = parseInt(input.question("Digite uma opção: "))
+            while(optDev !== 0){
+                console.log(`
+                Bem vindo desenvolvedor
+            
+                Escolha uma das opções abaixo, caso não queira alterar direto no banco.
+            
+                1 - Criar nova empresa
+                2 - Alterar senha de admin
+                0 - Sair
+                `)
 
-            switch(optDev){
+                optDev = parseInt(input.question("Digite uma opção: "))
+
+                switch(optDev){
+                    case 1:
+                        let company = input.question("Nome da empresa: ");
+                        if(verifyIfCompanyExists(companiesList, company)){
+                            console.log("Company already in database")
+                        } else {
+                            let name =  input.question("Nome do funcionário: ");
+                            let email = input.question("Digite o e-mail: ");
+                            let password = input.question("Digite uma senha: "); 
+                
+                            const novaEmpresa = new Company (company, name, email, password)
+                
+                            novaEmpresa.register()
+                        }
+                        break
+
+                    case 2:
+                        console.log('Alterar senha de admin')
+                        break
+                    
+                    case 0:
+                        console.log('Encerrando sessão')
+                        console.clear()
+                        break
+                }
+            }
+        break
+
+        case 'userAdmin':
+
+
+            let optAdmin = 1
+
+            while(optAdmin !== 0){
+                console.log(` 
+                
+
+                Menu Inicial
+                
+                1 - Listar funcionários
+                2 - Inserir funcionário
+                3 - Remover funcionário
+                0 - Sair
+                \n`)
+
+                optAdmin = parseInt(input.question("Digite uma opção: "))
+
+                switch(optAdmin){
                 case 1:
-                    let company = input.question("Nome da empresa: ");
-                    if(verifyIfCompanyExists(companiesList, company)){
-                        console.log("Company already in database")
+                    console.log('Menu lista funcionarios')
+
+                case 2:
+                    let funcCompany = input.question("Digite o nome da Empresa: ") // Aqui pode receber de acordo com o Login e não precisa mais validar
+                    if(verifyIfCompanyExists(companiesList, funcCompany)){
+                        let funcName = input.question("Digite o seu nome: ")
+                        let funcEmail = input.question("Digite o e-mail: ")
+                        let funcPassword = input.question("Digite uma senha: ")
+
+                        const funcionario = new Employee(funcCompany, funcName, funcEmail, funcPassword)
+                        funcionario.register()
                     } else {
-                        let name =  input.question("Nome do funcionário: ");
-                        let email = input.question("Digite o e-mail: ");
-                        let password = input.question("Digite uma senha: "); 
-            
-                        const novaEmpresa = new Company (company, name, email, password)
-            
-                        novaEmpresa.register()
+                        console.log("Company not found")
                     }
                     break
 
-                case 2:
-                    console.log('Alterar senha de admin')
-                    break
-                
-                case 0:
-                    console.log('Encerrando sessão')
-                    console.clear()
-                    break
-            }
-        }
-    }
-
-    else if (emailAuth === 'a@b.com' && passAuth === '123'){
-
-
-        let optAdmin = 1
-
-        while(optAdmin !== 0){
-            console.log(` 
-            
-
-            Menu Inicial
-            
-            1 - Listar funcionários
-            2 - Inserir funcionário
-            3 - Remover funcionário
-            0 - Sair
-            \n`)
-
-            optAdmin = parseInt(input.question("Digite uma opção: "))
-
-            switch(optAdmin){
-            case 1:
-                console.log('Menu lista funcionarios')
-
-            case 2:
-                let funcCompany = input.question("Digite o nome da Empresa: ") // Aqui pode receber de acordo com o Login e não precisa mais validar
-                if(verifyIfCompanyExists(companiesList, funcCompany)){
-                    let funcName = input.question("Digite o seu nome: ")
-                    let funcEmail = input.question("Digite o e-mail: ")
-                    let funcPassword = input.question("Digite uma senha: ")
-
-                    const funcionario = new Employee(funcCompany, funcName, funcEmail, funcPassword)
-                    funcionario.register()
-                } else {
-                    console.log("Company not found")
-                }
-                break
-
-            case 3:
-                console.log('Menu remover funcionario')
-                break
-
-            case 0:
-                console.log('Encerrando sessão')
-                console.clear()
-                break
-
-            default:
-                console.log("\nOpção invalida.")
-                break
-            }
-        }
-    }
-
-    else if (emailAuth === 'func@b.com' && passAuth === '123'){
-
-        let optFunc = 1
-
-        while(optFunc !== 0){
-            console.log(`
-                1 - Registrar ponto (input/saida)
-                2 - Consultar registros
-                3 - Consultar banco de horas
-                4 - Alterar senha
-                0 - Sair
-            `)
-            optFunc = parseInt(input.question("Digite uma opção: "))
-
-            switch(optFunc){
-                case 1:
-                    console.log('Registrar ponto')
-                    break
-                
-                case 2:
-                    console.log('Consultar Registros')
-                    break
                 case 3:
-                    console.log('Consultar banco de horas')
+                    console.log('Menu remover funcionario')
                     break
-                case 4:
-                    console.log('Alterar senha')
-                    break
+
                 case 0:
                     console.log('Encerrando sessão')
                     console.clear()
                     break
+
                 default:
-                    console.log('Opcao invalida')
+                    console.log("\nOpção invalida.")
                     break
+                }
             }
-        }
+        break
+
+        case 'userEmployee':
+
+            let optFunc = 1
+
+            while(optFunc !== 0){
+                console.log(`
+                    1 - Registrar ponto (input/saida)
+                    2 - Consultar registros
+                    3 - Consultar banco de horas
+                    4 - Alterar senha
+                    0 - Sair
+                `)
+                optFunc = parseInt(input.question("Digite uma opção: "))
+
+                switch(optFunc){
+                    case 1:
+                        console.log('Registrar ponto')
+                        break
+                    
+                    case 2:
+                        console.log('Consultar Registros')
+                        break
+                    case 3:
+                        console.log('Consultar banco de horas')
+                        break
+                    case 4:
+                        console.log('Alterar senha')
+                        break
+                    case 0:
+                        console.log('Encerrando sessão')
+                        console.clear()
+                        break
+                    default:
+                        console.log('Opcao invalida')
+                        break
+                }
+            }
+        break
+
+        default:
+            console.log('User not found! Try again')
+        break
     }
 
 }
